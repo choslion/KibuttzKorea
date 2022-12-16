017acbcad6aea94b638a0ff93befc600e290040b
Eddy <cshyeok24@gmail.com>
2022-12-16 17:47:34 +0900

    www.js 변경

--- bin/www.js
+++ bin/www.js
@@ -1,11 +1,13 @@
 //  실행폴더 bin
-let app = require("../app");
+let app = require('../app');
 
 let port = process.env.PORT || 3005;
 
-app.listen(port, () => {
-  console.log(`${port} 로 express 실행`);
-});
+// app.listen(port, () => {
+//   console.log(`${port} 로 express 실행`);
+// });
 //  실행파일
 
 //
+// 헤로쿠용
+app.listen(process.env.PORT || port);
