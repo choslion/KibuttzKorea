1dacbb4600398068b13c7457edf1fc3f65beec5a
Eddy <cshyeok24@gmail.com>
2023-02-03 09:49:24 +0900

    경로수정

--- bin/www.js
+++ bin/www.js
@@ -1,14 +1,13 @@
 //  실행폴더 bin
 let app = require('../app');
 
-// let PORT = process.env.PORT || 3005;
-let PORT = 3005;
+let PORT = process.env.PORT || 3005;
+// let PORT = 3005;
 
-app.listen(process.env.PORT || PORT);
-// app.listen(PORT, () => {
-//   console.log(`http://localhost:${PORT} 로 실행`);
-// });
-//  실행파일
+// app.listen(process.env.PORT || PORT);
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT} 로 실행`);
+});
 
 //
 // app.listen(PORT, () => {
