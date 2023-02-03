973d0723caf6d875622c46305339b53e104bb52a
Eddy <cshyeok24@gmail.com>
2023-02-03 09:43:25 +0900

    파일수정 www수정

--- bin/www.js
+++ bin/www.js
@@ -4,10 +4,10 @@ let app = require('../app');
 // let PORT = process.env.PORT || 3005;
 let PORT = 3005;
 
-// app.listen(process.env.PORT || PORT);
-app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT} 로 실행`);
-});
+app.listen(process.env.PORT || PORT);
+// app.listen(PORT, () => {
+//   console.log(`http://localhost:${PORT} 로 실행`);
+// });
 //  실행파일
 
 //
